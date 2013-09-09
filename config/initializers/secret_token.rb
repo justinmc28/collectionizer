33283f50f1a7d8e306370f6d4e65184ba20552e0
Justin McLaughlin <justin@boxfirepress.com>
2013-09-09 14:21:43 -0400

    thought this was done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Collectionizer::Application.config.secret_key_base = '6e6faa419059515ebdc1823a120762a689a52fee07cccc95107e87f0922fdba59bf343b94941962ba31b440c36db448b0f8600c7309ffb8b367f8a07a12d08bf'
