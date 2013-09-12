40b8bcb3b0f78b903c9c5bfa88f7924f7697b4b6
Justin McLaughlin <justin@boxfirepress.com>
2013-09-12 17:16:17 -0400

    fb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,5 +5,12 @@ module ApplicationHelper
      precise_total = number_with_precision(total, :precision => 2)
      "$#{precise_total}"
    end
+   
+   def money_no_sign(cents)
+      total = cents.to_f / 100
+      precise_total = number_with_precision(total, :precision => 2)
+      "#{precise_total}"
+   end
+   
   
 end
