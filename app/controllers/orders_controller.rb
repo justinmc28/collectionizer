e604a634495ec31219e4c402bfac84ff74a9a40f
Justin McLaughlin <justin@boxfirepress.com>
2013-09-13 11:56:06 -0400

    remove tracking for old orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -22,6 +22,7 @@ class OrdersController < ApplicationController
     @order.card = params[:stripeToken]
     @order.amount_in_cents = @order.amount * 100
       if @order.save_with_payment
+        session[:order_time] = Time.now;
         redirect_to @order
       else
         render action: "new", error: "There was a problem. Please try again."
